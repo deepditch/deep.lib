1db1378c32e7ff008f7df616ecfeea4ee09085d0
drakesvoboda <drakesvoboda@gmail.com>
2020-01-24 15:15:57 -0500

    Added shake drop pyramid net

--- Models/ShakeDrop/shake_pyramidnet.py
+++ Models/ShakeDrop/shake_pyramidnet.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-from models.shakedrop import ShakeDrop
+from Models.ShakeDrop.shakedrop import ShakeDrop
 
 
 class ShakeBasicBlock(nn.Module):
