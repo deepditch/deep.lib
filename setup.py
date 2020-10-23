04310d7b57ff3e51d6b0a3b4b5a8c7ad200f2338
drakesvoboda <drakesvoboda@gmail.com>
2020-10-23 15:42:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='deeplib', version='1.0', packages=find_packages('deeplib'))
+setup(name='deeplib', version='1.0', packages=find_packages(include=['deeplib', 'deeplib.*']))
