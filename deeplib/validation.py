bca6703716dc687fbaf61dffbf2c2674fff93fb1
drakesvoboda <drakesvoboda@gmail.com>
2020-11-14 12:41:20 -0500

    Fix device issue with validator callback

--- deeplib/validation.py
+++ deeplib/validation.py
@@ -196,7 +196,7 @@ class Validator(TrainCallback):
                 else:
                     label = Variable(util.to_gpu(label))
 
-                output, input = session.forward(input)
+                output, _ = session.forward(input)
                 step_loss = session.criterion(output, label).data
                 valLoss.update(step_loss, label.shape[0])
                 if self.accuracy_meter is not None:        
