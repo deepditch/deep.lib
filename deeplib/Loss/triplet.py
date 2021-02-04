5680ea0988597e2e49023994df4912e5e5d558cb
Drake Svoboda <drake@thinkmoncur.com>
2021-02-04 11:58:43 -0500

    Allow custom pairwise distance functions for triplet loss

--- deeplib/Loss/triplet.py
+++ deeplib/Loss/triplet.py
@@ -11,6 +11,98 @@ from torch.autograd import Variable
 import deeplib.util as util
 
 
+def pairwise_distances(embeddings, squared=False):
+    """Compute the 2D matrix of distances between all the embeddings.
+    Args:
+        embeddings: tensor of shape (batch_size, embed_dim)
+        squared: Boolean. If true, output is the pairwise squared euclidean distance matrix.
+                 If false, output is the pairwise euclidean distance matrix.
+    Returns:
+        pairwise_distances: tensor of shape (batch_size, batch_size)
+    """
+    dot_product = torch.matmul(embeddings, embeddings.t())
+
+    # Get squared L2 norm for each embedding. We can just take the diagonal of `dot_product`.
+    # This also provides more numerical stability (the diagonal of the result will be exactly 0).
+    # shape (batch_size,)
+    square_norm = torch.diag(dot_product)
+
+    # Compute the pairwise distance matrix as we have:
+    # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
+    # shape (batch_size, batch_size)
+    distances = square_norm.unsqueeze(0) - 2.0 * dot_product + square_norm.unsqueeze(1)
+
+    # Because of computation errors, some distances might be negative so we put everything >= 0.0
+    distances[distances < 0] = 0
+
+    if not squared:
+        # Because the gradient of sqrt is infinite when distances == 0.0 (ex: on the diagonal)
+        # we need to add a small epsilon where distances == 0.0
+        mask = distances.eq(0).float()
+        distances = distances + mask * 1e-16
+
+        distances = (1.0 -mask) * torch.sqrt(distances)
+
+    return distances
+
+def _get_triplet_mask(labels):
+    """Return a 3D mask where mask[a, p, n] is True iff the triplet (a, p, n) is valid.
+    A triplet (i, j, k) is valid if:
+        - i, j, k are distinct
+        - labels[i] == labels[j] and labels[i] != labels[k]
+    Args:
+        labels: tf.int32 `Tensor` with shape [batch_size]
+    """
+    # Check that i, j and k are distinct
+    indices_equal = util.to_gpu(torch.eye(labels.size(0)))
+    indices_not_equal = indices_equal != 1
+    i_not_equal_j = indices_not_equal.unsqueeze(2)
+    i_not_equal_k = indices_not_equal.unsqueeze(1)
+    j_not_equal_k = indices_not_equal.unsqueeze(0)
+
+    distinct_indices = (i_not_equal_j & i_not_equal_k) & j_not_equal_k
+
+
+    label_equal = labels.unsqueeze(0) == labels.unsqueeze(1)
+    i_equal_j = label_equal.unsqueeze(2)
+    i_equal_k = label_equal.unsqueeze(1)
+
+    valid_labels = ~i_equal_k & i_equal_j
+
+    return valid_labels & distinct_indices
+
+
+def _get_anchor_positive_triplet_mask(labels):
+    """Return a 2D mask where mask[a, p] is True iff a and p are distinct and have same label.
+    Args:
+        labels: tf.int32 `Tensor` with shape [batch_size]
+    Returns:
+        mask: tf.bool `Tensor` with shape [batch_size, batch_size]
+    """
+    # Check that i and j are distinct
+    indices_equal = util.to_gpu(torch.eye(labels.size(0)))
+    indices_not_equal = indices_equal != 1
+
+    # Check if labels[i] == labels[j]
+    # Uses broadcasting where the 1st argument has shape (1, batch_size) and the 2nd (batch_size, 1)
+    labels_equal = labels.unsqueeze(0) == labels.unsqueeze(1)
+
+    return labels_equal & indices_not_equal
+
+
+def _get_anchor_negative_triplet_mask(labels):
+    """Return a 2D mask where mask[a, n] is True iff a and n have distinct labels.
+    Args:
+        labels: tf.int32 `Tensor` with shape [batch_size]
+    Returns:
+        mask: tf.bool `Tensor` with shape [batch_size, batch_size]
+    """
+    # Check if labels[i] != labels[k]
+    # Uses broadcasting where the 1st argument has shape (1, batch_size) and the 2nd (batch_size, 1)
+
+    return ~(labels.unsqueeze(0) == labels.unsqueeze(1))
+
+
 def batch_hard_triplet_loss(embeddings, labels, margin, pairwise_distance_fn=pairwise_distances):
     """Build the triplet loss over a batch of embeddings.
     For each anchor, we get the hardest positive and hardest negative to form a triplet.
@@ -203,94 +295,3 @@ class DistanceWeightedMarginLoss(nn.Module):
   def forward(self, x, y):
     margin_loss = distance_weighted_margin_loss(x, y, self.margin, self.beta, self.nonzero_loss_cutoff, self.cutoff) 
     return margin_loss
-
-def pairwise_distances(embeddings, squared=False):
-    """Compute the 2D matrix of distances between all the embeddings.
-    Args:
-        embeddings: tensor of shape (batch_size, embed_dim)
-        squared: Boolean. If true, output is the pairwise squared euclidean distance matrix.
-                 If false, output is the pairwise euclidean distance matrix.
-    Returns:
-        pairwise_distances: tensor of shape (batch_size, batch_size)
-    """
-    dot_product = torch.matmul(embeddings, embeddings.t())
-
-    # Get squared L2 norm for each embedding. We can just take the diagonal of `dot_product`.
-    # This also provides more numerical stability (the diagonal of the result will be exactly 0).
-    # shape (batch_size,)
-    square_norm = torch.diag(dot_product)
-
-    # Compute the pairwise distance matrix as we have:
-    # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
-    # shape (batch_size, batch_size)
-    distances = square_norm.unsqueeze(0) - 2.0 * dot_product + square_norm.unsqueeze(1)
-
-    # Because of computation errors, some distances might be negative so we put everything >= 0.0
-    distances[distances < 0] = 0
-
-    if not squared:
-        # Because the gradient of sqrt is infinite when distances == 0.0 (ex: on the diagonal)
-        # we need to add a small epsilon where distances == 0.0
-        mask = distances.eq(0).float()
-        distances = distances + mask * 1e-16
-
-        distances = (1.0 -mask) * torch.sqrt(distances)
-
-    return distances
-
-def _get_triplet_mask(labels):
-    """Return a 3D mask where mask[a, p, n] is True iff the triplet (a, p, n) is valid.
-    A triplet (i, j, k) is valid if:
-        - i, j, k are distinct
-        - labels[i] == labels[j] and labels[i] != labels[k]
-    Args:
-        labels: tf.int32 `Tensor` with shape [batch_size]
-    """
-    # Check that i, j and k are distinct
-    indices_equal = util.to_gpu(torch.eye(labels.size(0)))
-    indices_not_equal = indices_equal != 1
-    i_not_equal_j = indices_not_equal.unsqueeze(2)
-    i_not_equal_k = indices_not_equal.unsqueeze(1)
-    j_not_equal_k = indices_not_equal.unsqueeze(0)
-
-    distinct_indices = (i_not_equal_j & i_not_equal_k) & j_not_equal_k
-
-
-    label_equal = labels.unsqueeze(0) == labels.unsqueeze(1)
-    i_equal_j = label_equal.unsqueeze(2)
-    i_equal_k = label_equal.unsqueeze(1)
-
-    valid_labels = ~i_equal_k & i_equal_j
-
-    return valid_labels & distinct_indices
-
-
-def _get_anchor_positive_triplet_mask(labels):
-    """Return a 2D mask where mask[a, p] is True iff a and p are distinct and have same label.
-    Args:
-        labels: tf.int32 `Tensor` with shape [batch_size]
-    Returns:
-        mask: tf.bool `Tensor` with shape [batch_size, batch_size]
-    """
-    # Check that i and j are distinct
-    indices_equal = util.to_gpu(torch.eye(labels.size(0)))
-    indices_not_equal = indices_equal != 1
-
-    # Check if labels[i] == labels[j]
-    # Uses broadcasting where the 1st argument has shape (1, batch_size) and the 2nd (batch_size, 1)
-    labels_equal = labels.unsqueeze(0) == labels.unsqueeze(1)
-
-    return labels_equal & indices_not_equal
-
-
-def _get_anchor_negative_triplet_mask(labels):
-    """Return a 2D mask where mask[a, n] is True iff a and n have distinct labels.
-    Args:
-        labels: tf.int32 `Tensor` with shape [batch_size]
-    Returns:
-        mask: tf.bool `Tensor` with shape [batch_size, batch_size]
-    """
-    # Check if labels[i] != labels[k]
-    # Uses broadcasting where the 1st argument has shape (1, batch_size) and the 2nd (batch_size, 1)
-
-    return ~(labels.unsqueeze(0) == labels.unsqueeze(1))
